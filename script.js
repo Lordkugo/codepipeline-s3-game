3dbf728af5b06fa5e0c16e5bd6f9bea570d6e33a
Kingsley Ugochukwu <64180636+Lordkugo@users.noreply.github.com>
2023-11-27 13:32:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Constants
+    const CARD_BACK_IMAGE = 'images/blank.png';
+    const LOADING_SCREEN_TIMEOUT = 1000;
+
+    // Variables
     const grid = document.querySelector('#game-board');
     const startButton = document.getElementById('start-game');
     let cardsChosen = [];
     let cardsChosenId = [];
     let cardsWon = [];
 
+    // Card array and other constants...
     const cardArray = [
         { name: 'card1', img: 'images/distracted.png' },
         { name: 'card1', img: 'images/distracted.png' },
@@ -19,24 +25,31 @@ document.addEventListener('DOMContentLoaded', () => {
         // ...add more pairs as needed
     ];
 
+
+    // Function to shuffle array
     function shuffle(array) {
         array.sort(() => 0.5 - Math.random());
     }
 
+    // Function to create the board
     function createBoard() {
+        showLoadingScreen();
         shuffle(cardArray);
         grid.innerHTML = '';
         cardsWon = [];
 
         for (let i = 0; i < cardArray.length; i++) {
             const card = document.createElement('img');
-            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('src', CARD_BACK_IMAGE);
             card.setAttribute('data-id', i);
             card.addEventListener('click', flipCard);
             grid.appendChild(card);
         }
+
+        hideLoadingScreen();
     }
 
+    // Function to flip a card
     function flipCard() {
         let cardId = this.getAttribute('data-id');
         if (!cardsChosenId.includes(cardId)) {
@@ -44,11 +57,12 @@ document.addEventListener('DOMContentLoaded', () => {
             cardsChosenId.push(cardId);
             this.setAttribute('src', cardArray[cardId].img);
             if (cardsChosen.length === 2) {
-                setTimeout(checkForMatch, 500);
+                setTimeout(checkForMatch, 700);
             }
         }
     }
 
+    // Function to check for card match
     function checkForMatch() {
         const cards = document.querySelectorAll('#game-board img');
         const firstCardId = cardsChosenId[0];
@@ -61,17 +75,41 @@ document.addEventListener('DOMContentLoaded', () => {
             cards[secondCardId].removeEventListener('click', flipCard);
             cardsWon.push(cardsChosen);
         } else {
-            cards[firstCardId].setAttribute('src', 'images/blank.png');
-            cards[secondCardId].setAttribute('src', 'images/blank.png');
+            cards[firstCardId].setAttribute('src', CARD_BACK_IMAGE);
+            cards[secondCardId].setAttribute('src', CARD_BACK_IMAGE);
         }
 
         cardsChosen = [];
         cardsChosenId = [];
 
         if (cardsWon.length === cardArray.length / 2) {
-            alert('Congratulations! You found them all!');
+            showGameCompletionMessage();
         }
     }
 
+    // Function to show a loading screen
+    function showLoadingScreen() {
+        // ... Logic to show loading screen ...
+    }
+
+    // Function to hide the loading screen
+    function hideLoadingScreen() {
+        // ... Logic to hide loading screen ...
+    }
+
+    // Function to show game completion message
+    function showGameCompletionMessage() {
+        alert('Congratulations! You found them all!');
+    }
+
+    // Event listener for card clicks using event delegation
+    grid.addEventListener('click', (event) => {
+        const card = event.target.closest('img');
+        if (card && !cardsChosenId.includes(card.getAttribute('data-id'))) {
+            flipCard.call(card);
+        }
+    });
+
+    // Event listener for the start button
     startButton.addEventListener('click', createBoard);
 });
